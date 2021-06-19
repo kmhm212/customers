486c66c9fc0971496ae731ee569e732c45e4a5fe
kmhm212 <hmhm_auau@yahoo.co.jp>
2021-06-19 17:47:02 +0900

    顧客情報の編集、削除機能実装

--- functions.php
+++ functions.php
@@ -49,9 +49,79 @@ function addCustomer($company, $name, $email)
     $stmt->execute();
 }
 
+function findCustomerForId($id)
+{
+    $dbh = connectDb();
+    $sql = <<<EOM
+        SELECT
+            *
+        FROM
+            customers
+        WHERE
+            id = :id;
+    EOM;
+    $stmt = $dbh->prepare($sql);
+    $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+    $stmt->execute();
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
+}
+
+function editCustomer($id, $company, $name, $email)
+{
+    $dbh = connectDb();
+    $sql = <<<EOM
+        UPDATE
+            customers
+        SET
+            company = :company,
+            name = :name,
+            email = :email
+        WHERE
+            id = :id;
+    EOM;
+    $stmt = $dbh->prepare($sql);
+    $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+    $stmt->bindParam(':company', $company, PDO::PARAM_STR);
+    $stmt->bindParam(':name', $name, PDO::PARAM_STR);
+    $stmt->bindParam(':email', $email, PDO::PARAM_STR);
+    $stmt->execute();
+}
+
+function deleteCustomer($id)
+{
+    $dbh = connectDb();
+    $sql = <<<EOM
+        DELETE FROM
+            customers
+        WHERE
+            id = :id;
+    EOM;
+    $stmt = $dbh->prepare($sql);
+    $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+    $stmt->execute();
+}
+
 function insertValidate($company, $name, $email)
 {
     $validates = [];
+    if (empty($company)) {
+        $validates[] = MSG_COMPANY_REQUIRED;
+    }
+    if (empty($name)) {
+        $validates[] = MSG_NAME_REQUIRED;
+    }
+    if (empty($email)) {
+        $validates[] = MSG_EMAIL_REQUIRED;
+    }
+    return $validates;
+}
+
+function insertEditValidate($customer, $company, $name, $email)
+{
+    $validates = [];
+    if ($customer['company'] == $company && $customer['name']  == $name && $customer['email'] == $email) {
+        $validates[] = MSG_NO_CHANGE;
+    }
     if (empty($company)) {
         $validates[] = MSG_COMPANY_REQUIRED;
     }
